99cdde0e8e802c0c07003eb5ac12de1c8626df9c
Peter Timpone <petetimp@scarletmail.rutgers.edu>
2015-05-06 01:16:24 -0400

    boilerplate

--- index.php
+++ index.php
@@ -1,22 +1,53 @@
 <!doctype html>
-<html>
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>My Portfolio</title>
-       	<link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="icon" type="image/ico" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
-    
     <body>
-    <div id='img-div'><img src="img/Portfolio.png"></div>
-    <div id='link-wrapper'>
-    	<div class='click-a' id='one' onClick="location.href='http://cense-ppz.rhcloud.com/'"></div>
-    	<div class='click-a' id='two' onClick="location.href='http://paulspetzone-ppz.rhcloud.com/'"></div>
-    	<div class='click-a' id='three' onClick="location.href='https://magic.piktochart.com/output/3584951-driverless-car-approval'"></div>
-    	<div class='click-a' id='four' onClick="location.href='http://votewisetest.info/'"></div>
-    	<div class='click-b' id='five' onClick="location.href='ResumeExtended.pdf'"></div>
-    	<div class='click-b' id='six' onClick="location.href='https://github.com/petetimp?tab=repositories'"></div>
-        <div class='click-b' id='seven' onClick="location.href='https://www.linkedin.com/pub/peter-timpone/4b/869/234'"></div>
-    </div>
-    <div id='footer'></div>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div id='img-div'><img src="img/Portfolio.png"></div>
+    	<div id='link-wrapper'>
+    		<div class='click-a' id='one' onClick="location.href='http://cense-ppz.rhcloud.com/'"></div>
+    		<div class='click-a' id='two' onClick="location.href='http://paulspetzone-ppz.rhcloud.com/'"></div>
+    		<div class='click-a' id='three' onClick="location.href='https://magic.piktochart.com/output/3584951-driverless-car-approval'"></div>
+    		<div class='click-a' id='four' onClick="location.href='http://votewisetest.info/'"></div>
+        	<div class='click-a' id='eight' onClick="location.href='https://prezi.com/rv17oloaqxbp/echelon-streetlight-improvements/'"></div>
+        	<div class='click-a' id='nine' onClick="location.href='https://prezi.com/luq6ylbkrzug/healthcare/'"></div>
+    		<div class='click-b' id='five' onClick="location.href='ResumeExtended.pdf'"></div>
+    		<div class='click-b' id='six' onClick="location.href='https://github.com/petetimp?tab=repositories'"></div>
+        	<div class='click-b' id='seven' onClick="location.href='https://www.linkedin.com/pub/peter-timpone/4b/869/234'"></div>
+    	</div>
+    	<div id='footer'></div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
     </body>
 </html>
---
+++ index1.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>My Portfolio</title>
+       	<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    
+    <body>
+    <div id='img-div'><img src="img/Portfolio.png"></div>
+    <div id='link-wrapper'>
+    	<div class='click-a' id='one' onClick="location.href='http://cense-ppz.rhcloud.com/'"></div>
+    	<div class='click-a' id='two' onClick="location.href='http://paulspetzone-ppz.rhcloud.com/'"></div>
+    	<div class='click-a' id='three' onClick="location.href='https://magic.piktochart.com/output/3584951-driverless-car-approval'"></div>
+    	<div class='click-a' id='four' onClick="location.href='http://votewisetest.info/'"></div>
+        <div class='click-a' id='eight' onClick="location.href='https://prezi.com/rv17oloaqxbp/echelon-streetlight-improvements/'"></div>
+        <div class='click-a' id='nine' onClick="location.href='https://prezi.com/luq6ylbkrzug/healthcare/'"></div>
+    	<div class='click-b' id='five' onClick="location.href='ResumeExtended.pdf'"></div>
+    	<div class='click-b' id='six' onClick="location.href='https://github.com/petetimp?tab=repositories'"></div>
+        <div class='click-b' id='seven' onClick="location.href='https://www.linkedin.com/pub/peter-timpone/4b/869/234'"></div>
+    </div>
+    <div id='footer'></div>
+    </body>
+</html>

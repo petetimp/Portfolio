3e92c9e3a11c944df9f68ee5a45fe76dc1ca65d9
Peter Timpone <petetimp@scarletmail.rutgers.edu>
2015-05-21 18:58:30 -0400

    52115

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(
+	function(){
+		
+		$('.click-a, .click-b').mouseenter(
+			function(){
+          		$(this).css('background-color' , '#D4AF37');
+				$(this).css('opacity','.5');
+			}
+        );
+		
+		$('.click-a, .click-b').mouseleave(
+			function(){
+          		$(this).css('opacity','0');
+			}
+        );
+		
+		$('.click-a, .click-b').click(
+			function(){
+          		$(this).css('background-color' , '#00FF00');
+				$(this).css('opacity','.5');
+			}
+        );
+		
+	}
+);
